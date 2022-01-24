01295af980c074dce1c4a86175f2f8446293165c
Jose Luis Matias Pineda <jose-luis.matias@atos.net>
2022-01-24 15:04:52 -0600

    Fix: Update the counter

--- components/StatusPool.js
+++ components/StatusPool.js
@@ -2,7 +2,7 @@ const StatusPool = ({ counting }) => {
     console.log( counting )
     return ( 
         <div className="p-4 container-status">
-            <h1 className="text-light">Uploaded: { counting } </h1>
+            <h2 className="text-light">Uploaded: { counting } </h2>
         </div>
      );
 }
