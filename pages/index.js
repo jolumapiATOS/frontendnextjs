f36b0f7a9d094041066ec2eab48960c139ac5367
Jose Luis Matias Pineda <jose-luis.matias@atos.net>
2022-02-18 12:52:32 -0600

    UPdate: The page is operational

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,34 @@
-import Head from 'next/head'
-import Button from 'react-bootstrap/Button';
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { socket }  from '../public/service.js';
 
 
 export default function Home() {
+  const [log, setLog] = useState('unknown');
+
+  useEffect(() => {
+    socket.on('username', () => {
+      setLog("known")
+    })
+
+    socket.on("disconnect", () => {
+      setLog("unknown") // false
+  });
+
+  }, [socket])
+
+
   return (
-    <>
+    <div>
       <Head>
         <title> UPgrade | Daily   </title>
         <meta name='keywords' content='Tech Services' />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <div className="p-4">
       
+      { log === "unknown" && <div className="p-4">
         <h1 className={styles.title}> UPgrade | Frontend </h1>
         <p>
             The purpose of this app is for you to give daily feedback on all of your
@@ -33,7 +47,34 @@ export default function Home() {
         <div id="img-logo" >
           <Image src="/logoApp.svg" height={200} width={200} alt="image-for-everyone" ></Image>
         </div>
-      </div>
-    </>
+      </div> }
+
+      { log === "known" && <div className="p-4">
+        <h1 className={styles.title}> Bienvenido </h1>
+        
+        <h4>Websocket</h4>
+        <p>The app keeps track of whether you are connected or not. However you can still play around with the app.
+          Every time the state changes it receives an update.
+        </p>
+
+        <h4>Databases</h4>
+        <p>We have two databases running. One on the client side storing all his activities. The other one is running </p>
+
+        <h4>Databases</h4>
+        <p>We have two databases running. One on the client side storing all his activities. The other one is running on the Cloud. 
+          When the user is connected the app reaches the backend and compares both versions. If the the one th user has is more recent
+          it updates Mongo.
+        </p>
+
+        <h4>PWA</h4>
+        <p>
+          The app is installable and fully operational even offline.
+        </p>
+
+        
+      </div>}
+
+
+    </div>
   )
 }
