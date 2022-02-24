e19e6e75ff98c9dddd7d4d753609d51f2f1910e8
Jose Luis Matias Pineda <jose-luis.matias@atos.net>
2022-02-23 20:56:17 -0600

    FIX: Changed the backend connection

--- public/service.js
+++ public/service.js
@@ -1,7 +1,7 @@
 import Swal from "sweetalert2";
 const { io } = require("socket.io-client");
-//const socket = io.connect("https://node-server-for-upgrade.herokuapp.com/");
-const socket = io.connect("http://localhost:8000/");
+const socket = io.connect("https://node-server-for-upgrade.herokuapp.com/");
+//const socket = io.connect("http://localhost:8000/");
 
 
 
